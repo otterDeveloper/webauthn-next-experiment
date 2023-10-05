dc4252b1255bc61bee893907d03d448408868297
Miguel Medina <miguel@medinagtz.com>
2023-10-05 17:49:04 -0600

    switch to appdir

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "~/styles/globals.css";
+export const metadata = {
+  title: "Webauthn Demo",
+  description: "Webauthn Demo",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen">{children}</body>
+    </html>
+  );
+}
