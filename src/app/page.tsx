6bb3d4f33005788d44c252bad35d5765faca4930
Miguel Medina <miguel@medinagtz.com>
2023-10-12 13:05:43 -0600

    add basic login ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,69 @@
 import Link from "next/link";
+import { Button } from "~/@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "~/@/components/ui/card";
+import { Input } from "~/@/components/ui/input";
+import { Label } from "~/@/components/ui/label";
+import {
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from "~/@/components/ui/tabs";
 
 export default function IndexPage() {
-    return(<main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        <span className="text-[hsl(280,100%,70%)]">Webauthn</span> Demo
-      </h1>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-        <Link
-          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href="https://create.t3.gg/en/usage/first-steps"
-          target="_blank"
-        >
-          <h3 className="text-2xl font-bold">First Steps →</h3>
-          <div className="text-lg">
-            Just the basics - Everything you need to know to set up your
-            database and authentication.
-          </div>
-        </Link>
-        <Link
-          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href="https://create.t3.gg/en/introduction"
-          target="_blank"
-        >
-          <h3 className="text-2xl font-bold">Documentation →</h3>
-          <div className="text-lg">
-            Learn more about Create T3 App, the libraries it uses, and how
-            to deploy it.
-          </div>
-        </Link>
-      </div>
-      <div className="flex flex-col items-center gap-2">
-        
-      </div>
-    </div>
-  </main>)
+	return (
+		<main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+					<span className="text-[hsl(280,100%,70%)]">Webauthn</span> Demo
+				</h1>
+				<Tabs defaultValue="register" className="min-w-[30%]">
+					<TabsList className="grid  w-full grid-cols-2">
+						<TabsTrigger value="signin">Sign In</TabsTrigger>
+						<TabsTrigger value="register">Register</TabsTrigger>
+					</TabsList>
+					<TabsContent value="register">
+						<Card>
+							<CardHeader>
+								<CardTitle>Register</CardTitle>
+								<CardDescription>
+									Register with a passkey or a FIDO2 Security Key
+								</CardDescription>
+							</CardHeader>
+							<CardContent className="space-y-2">
+								<div className="space-y-1">
+									<Label htmlFor="email">Email</Label>
+									<Input id="email" type="email" />
+								</div>
+								<Button>Register</Button>
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="signin">
+						<Card>
+							<CardHeader>
+								<CardTitle>Sign In</CardTitle>
+								<CardDescription>
+									Sign In with a passkey or a FIDO2 Security Key
+								</CardDescription>
+							</CardHeader>
+							<CardContent className="space-y-2">
+								<div className="space-y-1">
+									<Label htmlFor="email">Email</Label>
+									<Input id="email" type="email" />
+								</div>
+								<Button>Sign In</Button>
+							</CardContent>
+						</Card>
+					</TabsContent>
+				</Tabs>
+			</div>
+		</main>
+	);
 }
