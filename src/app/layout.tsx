5345174fcaf44a19750bcb00a17f06b1ebd4c88f
Miguel Medina <miguel@medinagtz.com>
2023-10-12 16:11:24 -0600

    set colors & theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className="min-h-screen">{children}</body>
     </html>
   );
