0a850b5f9e42164b89961287b0b42246241d2331
Miguel Medina <miguel@medinagtz.com>
2023-10-16 23:12:17 -0600

    add loading dialog

--- src/app/WebauthnLogin.tsx
+++ src/app/WebauthnLogin.tsx
@@ -23,6 +23,14 @@ import {
 	TabsTrigger,
 } from "~/@/components/ui/tabs";
 import { trpc } from "~/utils/apiApp";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
 
 export type EmailFieldProps = {
 	email: string;
@@ -46,72 +54,97 @@ export const SignOutButton = () => (
 
 const WebuthnLogin = () => {
 	const [email, setEmail] = useState("");
-	const generateOption = trpc.webauthn.generateRegistrationOptions.useMutation({
-		onSuccess: async ({ operation, options }) => {
-			const registrationPromise =
-				operation === "registration"
-					? startRegistration(options)
-					: startAuthentication(options);
+	const [isAwaitingWebauthn, setIsAwaitingWebauthn] = useState(false);
+	const { mutate, error, isLoading } =
+		trpc.webauthn.generateRegistrationOptions.useMutation({
+			onSuccess: async ({ operation, options }) => {
+				const registrationPromise =
+					operation === "registration"
+						? startRegistration(options)
+						: startAuthentication(options);
 
-			const result = await registrationPromise;
-			await signIn("webauthn", {
-				email,
-				payload: SuperJSON.stringify(result),
-			});
-		},
-	});
-	const errorDisplay = generateOption.error && (
-		<div className="text-red-500">
-			{generateOption.error.message}
-		</div>
+				setIsAwaitingWebauthn(true);
+				const result = await registrationPromise;
+				await signIn("webauthn", {
+					email,
+					payload: SuperJSON.stringify(result),
+				});
+				setIsAwaitingWebauthn(false);
+			},
+		});
+	const errorDisplay = error && (
+		<div className="text-red-500">{error.message}</div>
 	);
 	return (
-		<Tabs defaultValue="register" className="min-w-[30%]">
-			<TabsList className="grid  w-full grid-cols-2">
-				<TabsTrigger value="signin">Sign In</TabsTrigger>
-				<TabsTrigger value="register">Register</TabsTrigger>
-			</TabsList>
-			<TabsContent value="register">
-				<Card>
-					<CardHeader>
-						<CardTitle>Register</CardTitle>
-						<CardDescription>
-							Register with a passkey or a FIDO2 Security Key
+		<>
+			<Tabs defaultValue="register" className="min-w-[30%]">
+				<TabsList className="grid  w-full grid-cols-2">
+					<TabsTrigger value="signin">Sign In</TabsTrigger>
+					<TabsTrigger value="register">Register</TabsTrigger>
+				</TabsList>
+				<TabsContent value="register">
+					<Card>
+						<CardHeader>
+							<CardTitle>Register</CardTitle>
+							<CardDescription>
+								Register with a passkey or a FIDO2 Security Key
+								{errorDisplay}
+							</CardDescription>
+						</CardHeader>
+						<CardContent className="space-y-2">
+							<EmailField email={email} setEmail={setEmail} />
+							<Button
+								disabled={isLoading}
+								onClick={() => mutate({ email, operation: "registration" })}>
+								Register
+							</Button>
+						</CardContent>
+					</Card>
+				</TabsContent>
+				<TabsContent value="signin">
+					<Card>
+						<CardHeader>
+							<CardTitle>Sign In</CardTitle>
+							<CardDescription>
+								Sign In with a passkey or a FIDO2 Security Key
+							</CardDescription>
 							{errorDisplay}
-						</CardDescription>
-					</CardHeader>
-					<CardContent className="space-y-2">
-						<EmailField email={email} setEmail={setEmail} />
-						<Button
-							onClick={() =>
-								generateOption.mutate({ email, operation: "registration" })
-							}>
-							Register
-						</Button>
-					</CardContent>
-				</Card>
-			</TabsContent>
-			<TabsContent value="signin">
-				<Card>
-					<CardHeader>
-						<CardTitle>Sign In</CardTitle>
-						<CardDescription>
-							Sign In with a passkey or a FIDO2 Security Key
-						</CardDescription>
-						{errorDisplay}
-					</CardHeader>
-					<CardContent className="space-y-2">
-						<EmailField email={email} setEmail={setEmail} />
-						<Button
-							onClick={() =>
-								generateOption.mutate({ email, operation: "login" })
-							}>
-							Sign In
-						</Button>
-					</CardContent>
-				</Card>
-			</TabsContent>
-		</Tabs>
+						</CardHeader>
+						<CardContent className="space-y-2">
+							<EmailField email={email} setEmail={setEmail} />
+							<Button
+								disabled={isLoading}
+								onClick={() => mutate({ email, operation: "login" })}>
+								Sign In
+							</Button>
+						</CardContent>
+					</Card>
+				</TabsContent>
+			</Tabs>
+			<Dialog open={isLoading}>
+				<DialogContent>
+					<DialogHeader>
+						{isAwaitingWebauthn ? (
+							<>
+								<DialogTitle>
+									Follow the Instructions on your device
+								</DialogTitle>
+								<DialogDescription>
+									You will be prompted to authenticate with your device
+								</DialogDescription>
+							</>
+						) : (
+							<>
+								<DialogTitle>Loading</DialogTitle>
+								<DialogDescription>
+									Getting ready to authenticate you...
+								</DialogDescription>
+							</>
+						)}
+					</DialogHeader>
+				</DialogContent>
+			</Dialog>
+		</>
 	);
 };
 
