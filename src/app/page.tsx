cd6253cc564386da6b9af219855edbf9d1a26efc
Miguel Medina <miguel@medinagtz.com>
2023-10-16 21:49:06 -0600

    add success message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
+import { getAuth } from "~/utils/query";
 import WebAuthnLogin from "./WebauthnLogin";
 
-export default function IndexPage() {
-
-
+export default async function IndexPage() {
+	const auth = await getAuth();
 	return (
 		<main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
 					<span className="text-[hsl(280,100%,70%)]">Webauthn</span> Demo
 				</h1>
-				<WebAuthnLogin/>
+				{auth === null && <WebAuthnLogin />}
+				{auth && (
+					<div>
+						<h3>Welcome!</h3>
+						<p>You are logged in as {auth.user.email}</p>
+					</div>
+				)}
 			</div>
 		</main>
 	);
