e2beb5bbbf5bc12e9139d3ada624954b4b5de068
Miguel Medina <miguel@medinagtz.com>
2023-11-19 14:47:48 -0600

    fix type errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
 function handler(req: NextRequest, ctx: {params: {nextauth: string[]}}) {
-    // @ts-expect-error This thing is not accepting NextRequest and is expecting NextApiRequest for some reason
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return NextAuth(req,ctx, authOptions);
 }
